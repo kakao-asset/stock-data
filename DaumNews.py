ff18c7fb666b188f38276edbb3c72b172cd2dc2c
yn15 <68692931+yn15@users.noreply.github.com>
2022-11-07 14:57:49 +0900

    Daum News Crolling - newsId, title, summary, imageUrl & newsDetailURL

--- DaumNews.py
+++ DaumNews.py
@@ -10,10 +10,14 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36 OPR/58.0.3135.127'
 }
 
+detail_url = "https://finance.daum.net/quotes/A376180#news/stock/"
+
 response = requests.get(url_origin, headers=headers)
 jsonObj = response.json()
 
 for i in jsonObj['data'] :
     print(i['newsId'], i['title'], i['summary'], i['imageUrl'])
+    print(detail_url + i['newsId'])
+
 
 # print(jsonObj)
